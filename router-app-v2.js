9e347b1d3a06a9205329004c47e0624df91e6b23
Osvaldo Jimenez <onzfonz@users.noreply.github.com>
2023-01-30 23:02:13 -0800

    Update router-app-v2.js

--- router-app-v2.js
+++ router-app-v2.js
@@ -1,148 +1,157 @@
 import ReactDOM from 'react-dom/client'
-import { useState } from 'react'
+import {useState} from 'react'
 
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Navigate,
-  useParams,
-  useNavigate,
-  useMatch
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Navigate,
+    useParams,
+    useNavigate,
+    useMatch
 } from "react-router-dom"
 
 
 const Home = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-  </div>
+    <div>
+        <h2>227 Task app</h2>
+        <p>
+            Do you and your family have a lot of recurring tasks that you need to do every so often?<br/>
+            <em>Do you often forget to do those tasks?</em><br/>
+            Does someone else need to remind you to do that task, and are they often frustrated about it?<br/>
+            Do you need to prioritize some tasks over others?
+        </p>
+        <p><strong>Well this is the app for you!</strong>
+        </p>
+    </div>
 )
 
-const Note = ({ note }) => {
-
-  return (
-    <div>
-      <h2>{note.content}</h2>
-      <div>{note.user}</div>
-      <div><strong>{note.important ? 'important' : ''}</strong></div>
-    </div>
-  )
+const Task = ({ task }) => {
+    return (
+        <div>
+            <h2>{task.content}</h2>
+            <div>{task.user}</div>
+            <div><strong>{task.important ? 'important' : ''}</strong></div>
+        </div>
+    )
 }
 
-const Notes = ({ notes }) => (
-  <div>
-    <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
-  </div>
+const Tasks = ({tasks}) => (
+    <div>
+        <h2>Tasks</h2>
+        <ul>
+            {tasks.map(task =>
+                <li key={task.id}>
+                    <Link to={`/tasks/${task.id}`}>{task.content}</Link>
+                </li>
+            )}
+        </ul>
+    </div>
 )
 
 const Users = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <ul>
-      <li>Matti Luukkainen</li>
-      <li>Juha Tauriainen</li>
-      <li>Arto Hellas</li>
-    </ul>
-  </div>
+    <div>
+        <h2>227 tasks app</h2>
+        <ul>
+            <li>Tommy Tiger Jr</li>
+            <li>Khoury Graffiti Rock</li>
+            <li>Robert Burns</li>
+        </ul>
+    </div>
 )
 
 const Login = (props) => {
-  const navigate = useNavigate()
+    const navigate = useNavigate()
 
-  const onSubmit = (event) => {
-    event.preventDefault()
-    props.onLogin('mluukkai')
-    navigate('/')
-  }
+    const onSubmit = (event) => {
+        event.preventDefault()
+        props.onLogin('powercat')
+        navigate('/')
+    }
 
-  return (
-    <div>
-      <h2>login</h2>
-      <form onSubmit={onSubmit}>
+    return (
         <div>
-          username: <input />
+            <h2>login</h2>
+            <form onSubmit={onSubmit}>
+                <div>
+                    username: <input/>
+                </div>
+                <div>
+                    password: <input type='password'/>
+                </div>
+                <button type="submit">login</button>
+            </form>
         </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      content: 'HTML is easy',
-      important: true,
-      user: 'Matti Luukkainen'
-    },
-    {
-      id: 2,
-      content: 'Browser can execute only JavaScript',
-      important: false,
-      user: 'Matti Luukkainen'
-    },
-    {
-      id: 3,
-      content: 'Most important methods of HTTP-protocol are GET and POST',
-      important: true,
-      user: 'Arto Hellas'
-    }
-  ])
-
-  const [user, setUser] = useState(null)
-
-  const match = useMatch('/notes/:id')
-
-  const note = match
-    ? notes.find(note => note.id === Number(match.params.id))
-    : null
-
-
-  const login = (user) => {
-    setUser(user)
-  }
-
-  const padding = {
-    padding: 5
-  }
-
-  return (
-    <div>
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/notes">notes</Link>
-        <Link style={padding} to="/users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            content: 'Take out the trash',
+            important: true,
+            user: 'Tommy Tiger Jr'
+        },
+        {
+            id: 2,
+            content: 'Wash the dishes',
+            important: false,
+            user: 'Tommy Tiger Jr'
+        },
+        {
+            id: 3,
+            content: 'Hang out',
+            important: true,
+            user: 'Khoury Graffiti Rock'
         }
-      </div>
-      <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
-        <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <div>
-        <br />
-        <em>Note app, Department of Computer Science 2022</em>
-      </div>
-    </div>
-  )
+    ])
+
+    const [user, setUser] = useState(null)
+
+    const login = (user) => {
+        setUser(user)
+    }
+
+    const padding = {
+        padding: 5
+    }
+
+    const match = useMatch('/tasks/:id')
+    const task = match
+        ? tasks.find(task => task.id === Number(match.params.id))
+        : null
+
+    return (
+        <div>
+            <div>
+                <Link style={padding} to="/">home</Link>
+                <Link style={padding} to="/tasks">tasks</Link>
+                <Link style={padding} to="/users">users</Link>
+                {user
+                    ? <em>{user} logged in</em>
+                    : <Link style={padding} to="/login">login</Link>
+                }
+            </div>
+
+            <Routes>
+                <Route path="/tasks/:id" element={<Task task={task}/>}/>
+                <Route path="/tasks" element={<Tasks tasks={tasks}/>}/>
+                <Route path="/users" element={user ? <Users/> : <Navigate replace to="/login"/>}/>
+                <Route path="/login" element={<Login onLogin={login}/>}/>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+            <footer>
+                <br/>
+                <em>Task app, Department of Computer Science 2023</em>
+            </footer>
+        </div>
+    )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+        <App/>
+    </Router>
+)
