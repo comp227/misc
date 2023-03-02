a2590468c5500469c949f5e1cbc51ce3187266f6
Osvaldo Jimenez <onzfonz@users.noreply.github.com>
2023-03-02 10:11:47 -0800

    fixed typos for initial setup

--- patients.ts
+++ patients.ts
@@ -7,8 +7,7 @@ const patientEntries: Array<Patient> = [
         "dateOfBirth": "1986-07-09",
         "ssn": "090786-122X",
         "gender": "male",
-        "occupation": "threat to humanity",
-        "entries": []
+        "occupation": "threat to humanity"
     },
     {
         "id": "d2773598-f723-11e9-8f0b-362b9e155667",
@@ -16,8 +15,7 @@ const patientEntries: Array<Patient> = [
         "dateOfBirth": "1956-05-12",
         "ssn": "300179-77A",
         "gender": "male",
-        "occupation": "Nuclear Safety Inspector",
-        "entries": []
+        "occupation": "Nuclear Safety Inspector"
     },
     {
         "id": "d27736ec-f723-11e9-8f0b-362b9e155667",
@@ -25,8 +23,7 @@ const patientEntries: Array<Patient> = [
         "dateOfBirth": "1986-08-06",
         "ssn": "250470-555L",
         "gender": "female",
-        "occupation": "Bounty Hunter",
-        "entries": []
+        "occupation": "Bounty Hunter"
     },
     {
         "id": "d2773822-f723-11e9-8f0b-362b9e155667",
@@ -34,8 +31,7 @@ const patientEntries: Array<Patient> = [
         "dateOfBirth": "1992-10-21",
         "ssn": "050174-432N",
         "gender": "male",
-        "occupation": "Bad Guy",
-        "entries": []
+        "occupation": "Bad Guy"
     },
     {
         "id": "d2773c6e-f723-11e9-8f0b-362b9e155667",
@@ -43,8 +39,7 @@ const patientEntries: Array<Patient> = [
         "dateOfBirth": "2000-05-10",
         "ssn": "090471-8890",
         "gender": "male",
-        "occupation": "Race Car Driver",
-        "entries": []
+        "occupation": "Race Car Driver"
     }
 ];
 
